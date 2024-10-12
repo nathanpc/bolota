da8226cc385962c6718ca38d4ce07595b02ed6e2
Nathan Campos <nathan@innoveworkshop.com>
2024-10-12 13:25:27 +0100

    Started working on a field abstraction in C++

---
+++ src/Bolota/Field.cpp
@@ -0,0 +1,283 @@
+/**
+ * Field.cpp
+ * Field object abstraction of a Bolota document.
+ *
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+
+#include "Field.h"
+
+using namespace Bolota;
+
+/*
+ * +===========================================================================+
+ * |                                                                           |
+ * |                      Constructors and destructors                         |
+ * |                                                                           |
+ * +===========================================================================+
+ */
+
+/**
+ * Constructs a field object with a predefined type.
+ *
+ * @param type Type of the field.
+ */
+Field::Field(bolota_type_t type) {
+	Initialize(type, NULL, NULL, NULL, NULL, NULL);
+}
+
+/**
+ * Constructs a field object with a predefined type and a parent field.
+ *
+ * @param parent Parent field.
+ * @param type   Type of the field.
+ */
+Field::Field(Field *parent, bolota_type_t type) {
+	Initialize(type, NULL, parent, NULL, NULL, NULL);
+}
+
+/**
+ * Handles the object destruction and cleaning up of resources.
+ */
+Field::~Field() {
+	if (HasText())
+		delete m_text;
+}
+
+/**
+ * Performs the common initialization tasks of the object.
+ *
+ * @param type   Our field type.
+ * @param text   Text content of our field.
+ * @param parent Parent field object.
+ * @param child  Child field object.
+ * @param prev   Previous field object in the linked list.
+ * @param next   Next field object in the linked list.
+ */
+void Field::Initialize(bolota_type_t type, UString *text, Field *parent,
+					   Field *child, Field *prev, Field *next) {
+	// Set properties.
+	SetType(type);
+	m_text = text;
+
+	// Setup the linked list.
+	SetParent(parent);
+	SetChild(child);
+	SetPrevious(prev);
+	SetNext(next);
+}
+
+/*
+ * +===========================================================================+
+ * |                                                                           |
+ * |                           Getters and Setters                             |
+ * |                                                                           |
+ * +===========================================================================+
+ */
+
+/**
+ * Gets the field type.
+ *
+ * @return Field type.
+ */
+bolota_type_t Field::Type() const {
+	return m_type;
+}
+
+/**
+ * Sets the field type.
+ *
+ * @param type Field type.
+ */
+void Field::SetType(bolota_type_t type) {
+	m_type = type;
+}
+
+/**
+ * Gets the field depth based on its parent objects.
+ *
+ * @return Field depth.
+ */
+uint8_t Field::Depth() const {
+	return HasParent() ? (Parent()->Depth() + 1) : 0;
+}
+
+/**
+ * Checks if the field text exists.
+ *
+ * @returns TRUE if some text is associated with this field.
+ */
+bool Field::HasText() const {
+	return m_text != NULL;
+}
+
+/**
+ * Gets the field text.
+ *
+ * @return Field text if one is available, NULL otherwise.
+ */
+UString* Field::Text() const {
+	return m_text;
+}
+
+/**
+ * Sets the field's text component.
+ *
+ * @param mbstr Multi byte string of text to be associated with the field.
+ */
+void Field::SetText(const char *mbstr) {
+	if (HasText()) {
+		*m_text = mbstr;
+	} else {
+		m_text = new UString(mbstr);
+	}
+}
+
+/**
+ * Sets the field's text component.
+ *
+ * @param wstr Wide character string of text to be associated with the field.
+ */
+void Field::SetText(const wchar_t *wstr) {
+	if (HasText()) {
+		*m_text = wstr;
+	} else {
+		m_text = new UString(wstr);
+	}
+}
+
+/**
+ * Generates a struct with the information contained in the object.
+ *
+ * @warning Do not free or manipulate the text field. It's shared internally
+ *          with another object.
+ *
+ * @return A struct with the information from the object.
+ */
+bolota_field_t Field::Struct() const {
+	bolota_field_t field;
+
+	// Initialize the structure.
+	field.type = Type();
+	field.depth = Depth();
+	field.length = static_cast<uint16_t>(Text()->Length());
+	field.text = const_cast<char *>(Text()->GetMultiByteString());
+
+	return field;
+}
+
+/*
+ * +===========================================================================+
+ * |                                                                           |
+ * |                              Linked List                                  |
+ * |                                                                           |
+ * +===========================================================================+
+ */
+
+/**
+ * Checks if a parent field exists.
+ *
+ * @returns TRUE if a parent field is associated with this object.
+ */
+bool Field::HasParent() const {
+	return m_parent != NULL;
+}
+
+/**
+ * Gets the parent field of this object.
+ *
+ * @return Parent field.
+ */
+Field* Field::Parent() const {
+	return m_parent;
+}
+
+/**
+ * Sets the parent field of this object.
+ *
+ * @param parent Parent field.
+ */
+void Field::SetParent(Field *parent) {
+	m_parent = parent;
+}
+
+/**
+ * Checks if a child field exists.
+ *
+ * @returns TRUE if a child field is associated with this object.
+ */
+bool Field::HasChild() const {
+	return m_child != NULL;
+}
+
+/**
+ * Gets the child field of this object.
+ *
+ * @return Child field.
+ */
+Field* Field::Child() const {
+	return m_child;
+}
+
+/**
+ * Sets the child field of this object.
+ *
+ * @param child Child field.
+ */
+void Field::SetChild(Field *child) {
+	m_child = child;
+}
+
+/**
+ * Checks if a previous field exists.
+ *
+ * @returns TRUE if a previous field is associated with this object.
+ */
+bool Field::HasPrevious() const {
+	return m_prev != NULL;
+}
+
+/**
+ * Gets the previous field in the linked list.
+ *
+ * @return Previous field.
+ */
+Field* Field::Previous() const {
+	return m_prev;
+}
+
+/**
+ * Sets the previous field in the linked list.
+ *
+ * @param prev Previous field.
+ */
+void Field::SetPrevious(Field *prev) {
+	m_prev = prev;
+}
+
+/**
+ * Checks if a next field exists.
+ *
+ * @returns TRUE if a next field is associated with this object.
+ */
+bool Field::HasNext() const {
+	return m_next != NULL;
+}
+
+/**
+ * Gets the next field in the linked list.
+ *
+ * @return Next field.
+ */
+Field* Field::Next() const {
+	return m_next;
+}
+
+/**
+ * Sets the next field in the linked list.
+ *
+ * @param next Next field.
+ */
+void Field::SetNext(Field *next) {
+	m_next = next;
+}
---
+++ src/Bolota/Field.h
@@ -0,0 +1,107 @@
+/**
+ * Field.h
+ * Field object abstraction of a Bolota document.
+ *
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+
+#ifndef _BOLOTA_FIELD_H
+#define _BOLOTA_FIELD_H
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+#include "UString.h"
+
+extern "C" {
+#endif // __cplusplus
+
+/**
+ * An 8-bit value that determines the type of a field.
+ */
+typedef enum bolota_type_e {
+	BOLOTA_TYPE_IGNORE = 0,
+	BOLOTA_TYPE_TEXT = 'T',
+	BOLOTA_TYPE_DATE = 'd'
+} bolota_type_t;
+
+/**
+ * A line of a note in a document.
+ */
+typedef struct bolota_field_s {
+	/* Header */
+	bolota_type_t type;  /* Field type. (uint8) */
+	uint8_t depth;       /* Level of indentation of the topic in the note. */
+	uint16_t length;     /* Length of the data part of the field (does not
+                          * include header). */
+
+	/* Data */
+	union {
+		char *text;      /* Text associated with the field. (not NUL terminated */
+		uint8_t *data;   /* when saved to file) */
+	};
+} bolota_field_t;
+
+#ifdef __cplusplus
+}
+
+namespace Bolota {
+	/**
+	 * Field object abstraction of a Bolota document.
+	 */
+	class Field {
+	private:
+		// Internals
+		bolota_type_t m_type;
+		UString *m_text;
+
+		// Linked list.
+		Field *m_parent;
+		Field *m_child;
+		Field *m_prev;
+		Field *m_next;
+
+	public:
+		// Constructors and destructors.
+		Field(bolota_type_t type);
+		Field(Field *parent, bolota_type_t type);
+		virtual ~Field();
+
+		// Getters and setters.
+		bolota_type_t Type() const;
+		void SetType(bolota_type_t type);
+		uint8_t Depth() const;
+		bool HasText() const;
+		UString* Text() const;
+		void SetText(const char *mbstr);
+		void SetText(const wchar_t *wstr);
+		bolota_field_t Struct() const;
+
+		// Linked list.
+		bool HasParent() const;
+		Field* Parent() const;
+		void SetParent(Field *parent);
+		bool HasChild() const;
+		Field* Child() const;
+		void SetChild(Field *child);
+		bool HasPrevious() const;
+		Field* Previous() const;
+		void SetPrevious(Field *prev);
+		bool HasNext() const;
+		Field* Next() const;
+		void SetNext(Field *next);
+
+	protected:
+		// Constructor helper.
+		void Initialize(bolota_type_t type, UString *text, Field *parent,
+			Field *child, Field *prev, Field *next);
+	};
+}
+
+#endif // __cplusplus
+
+#endif // _BOLOTA_FIELD_H
