383cb94de82d7f9c3613a557d0af0e2ccbb91b2d
Nathan Campos <nathan@innoveworkshop.com>
2024-12-02 23:11:16 +0000

    Added field icons to the project

--- src/SharedResources.h
+++ src/SharedResources.h
@@ -47,4 +47,41 @@
 #define IDC_DP_EDTSUBTITLE              (IDD_DOCPROPS + 2)
 #define IDC_DP_DTPTIMESTAMP             (IDD_DOCPROPS + 3)
 
+// Icons and Bitmaps.
+#define IDI_BASE                        3000
+#define IDI_BLANK                       3000
+
+// Field icons.
+#define IDI_FI_BATTERY                  3001
+#define IDI_FI_BOX                      3002
+#define IDI_FI_CALENDAR                 3003
+#define IDI_FI_CAMERA                   3004
+#define IDI_FI_CHECK                    3005
+#define IDI_FI_CLIPBOARD                3006
+#define IDI_FI_CLOCK                    3007
+#define IDI_FI_CPU                      3008
+#define IDI_FI_FIND                     3009
+#define IDI_FI_FOLDER                   3010
+#define IDI_FI_GEAR                     3011
+#define IDI_FI_HELP                     3012
+#define IDI_FI_HISTORY                  3013
+#define IDI_FI_LAPTOP                   3014
+#define IDI_FI_LIGHT                    3015
+#define IDI_FI_LOVE                     3016
+#define IDI_FI_MEN                      3017
+#define IDI_FI_MONEY                    3018
+#define IDI_FI_MOVIE                    3019
+#define IDI_FI_PLUS                     3020
+#define IDI_FI_REDO                     3021
+#define IDI_FI_REMOVE                   3022
+#define IDI_FI_SIGNPOST                 3023
+#define IDI_FI_SOUND                    3024
+#define IDI_FI_STAR                     3025
+#define IDI_FI_STOP                     3026
+#define IDI_FI_TAGS                     3027
+#define IDI_FI_TRASH                    3028
+#define IDI_FI_UNDO                     3029
+#define IDI_FI_WOMAN                    3030
+#define IDI_FI_WRENCH                   3031
+
 #endif // _BOLOTA_SHARED_RESOURCES_H
